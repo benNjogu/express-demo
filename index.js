503a35ff03584b5afcc2aa9a6f3284fb5d893067
benNjogu <kamandemachafi@gmail.com>
2022-10-01 13:03:01 +0300

    Environment variables

--- index.js
+++ index.js
@@ -10,4 +10,5 @@ app.get("/api/courses", (req, res) => {
   res.send([1, 2, 3]);
 });
 
-app.listen(3001, () => console.log("Listening on port 3001"));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
