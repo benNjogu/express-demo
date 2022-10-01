0e55104c5268044d3f821c507f6b3ee2853dbe7c
benNjogu <kamandemachafi@gmail.com>
2022-10-01 17:16:04 +0300

    Input validation

--- index.js
+++ index.js
@@ -17,18 +17,26 @@ app.get("/api/courses", (req, res) => {
   res.send(courses);
 });
 
-app.get("/api/courses/:id", (req, res) => {
-  const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send("course with the given ID not found");
-  res.send(course);
-});
-
 app.post("/api/courses", (req, res) => {
+  if (!req.body.name || req.body.name.length < 3) {
+    //400 bad request
+    res
+      .status(400)
+      .send("Name is required and should be a minimum of 3 characters.");
+    return;
+  }
+
   const course = {
     id: courses.length + 1,
     name: req.body.name,
   };
   courses.push(course);
+  res.send(course);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("course with the given ID not found");
   res.send(course);
 });
 
