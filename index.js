f793a2b0f048b188c0316e78521ca704b36c539d
benNjogu <kamandemachafi@gmail.com>
2022-10-05 14:09:26 +0300

    environments

--- index.js
+++ index.js
@@ -7,14 +7,20 @@ const express = require("express");
 
 const app = express();
 
-console.log(`NODE_ENV: ${process.env.NODE_ENV}`); //undefined
-console.log(`app: ${app.get("env")}`); //development
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(helmet());
 app.use(morgan("tiny"));
+
+
+/**
+ * The below helps to know whether the app is running on development, testing or production environment
+ */
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"));
+  console.log("Morgan enabled");
+}
 
 app.use(logger);
 
